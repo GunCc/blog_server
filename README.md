b2cddb038c20036248a3bbb35b495dcdb07b2cb6
Mango <2859893460@qq.com>
2022-10-11 09:46:25 +0800

    docs:项目初始化完毕，增加api层、初始化、核心层等文件，详情看说明文件

--- README.md
+++ README.md
@@ -1,6 +1,10 @@
 # 个人博客服务员
-
-- controller: 控制器
+- api： api层
+- config：配置包
+> 会配置一个 config.yaml 一样的管理包
+- core：核心文件（主要做初始化）
+- global：全局对象
+- initialize：初始化内部函数
 - middlewares: 中间件
 - modules: 存储类
 - routers: 路由文件
--- main.go
+++ main.go
@@ -1,20 +1,7 @@
 package main
 
-import (
-	"fmt"
-	"net/http"
-
-	"github.com/gin-gonic/gin"
-)
+import "blog_server/core"
 
 func main() {
-	fmt.Println("ceshi")
-
-	r := gin.Default()
-	r.GET("/ping", func(c *gin.Context) {
-		c.JSON(http.StatusOK, gin.H{
-			"message": "pong",
-		})
-	})
-	r.Run() // listen and serve on 0.0.0.0:8080 (for windows "localhost:8080")
+	core.RunWindowsServer()
 }
